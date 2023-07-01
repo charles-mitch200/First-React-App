a0e21517a420df9acc8566fa47ee6cec4e233bdb
charles-mitch200 <charlesreign2@gmail.com>
2023-07-01 14:11:35 +0300

    Updates

---
+++ src/Book.js
@@ -0,0 +1,19 @@
+const Book = (props) => {
+  const { author, title, img, number } = props;
+  //   console.log(props);
+  console.log(number);
+  // const getSingleBook = () => {
+  //     getBook(id);
+  // };
+
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <span className="number">{`#${number + 1}`}</span>
+    </article>
+  );
+};
+
+export default Book;
---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
+export const books = [
+  {
+    author: "Rebecca Yarros",
+    title: "Iron Flame (The Empyrean, 2)",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Colleen Hoover",
+    title: "Too Late: Definitive Edition",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Robert Greene",
+    title: "The 48 Laws of Power",
+    img: img3,
+    id: 3,
+  },
+];
